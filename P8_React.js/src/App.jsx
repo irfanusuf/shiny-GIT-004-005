67a975ad49c583ee391af10e2cc7b1ffa22df52b
Irfan Yousuf <irfanusuf33@gmail.com>
2025-07-23 16:59:58 +0530

    fixes

--- P8_React.js/src/App.jsx
+++ P8_React.js/src/App.jsx
@@ -1,49 +1,49 @@
-
-
-import React, { useState } from 'react'
-
-
-
-
-
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(40);
+  const [explosionCounter, SetEXplosionCounter] = useState(10)
 
+  function handleIncrement() {
+    setCount((count) => count + 1);
+  }
 
-
-const [count , setCount] = useState(40) 
-
-
-
-
- function handleIncrement (){
-
-    setCount(count => count +1)
-
-}
-
-
- function handleDecrement  (){
-
-    if(count>0){
-   setCount(count => count -1)
+  function handleDecrement() {
+    if (count > 0) {
+      setCount((count) => count - 1);
     }
- 
+  }
 
-}
+
+  useEffect(() => {
+
+    SetEXplosionCounter((explosionCounter) => explosionCounter - 1)
+
+  } , [] )
+
+
+
   return (
-    <div><h1> bullets in the magazine {count} </h1>
-    
-        <button onClick={handleIncrement}> Increment </button>
+    <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", width: "50%", margin: "auto" }}>
+      <h1> bullets in the magazine {count} </h1>
 
-         <button onClick={handleDecrement}> Fire </button>
+
+
+      <button onClick={handleIncrement}> Increment </button>
+
+      <button onClick={handleDecrement}> Fire </button>
+
+
+
+      <h1>
+
+        Bomb planted :
+
+        <p> Explosion in {explosionCounter} seconds  </p>
+
+      </h1>
     </div>
-  )
+  );
+};
 
-
-
-}
-
-
-
-export default App
+export default App;
