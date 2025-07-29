fb87ee402511c32a9033e0185261155f76d633a6
Irfan Yousuf <irfanusuf33@gmail.com>
2025-07-29 17:08:35 +0530

    fixes

--- P8_React.js/src/App.jsx
+++ P8_React.js/src/App.jsx
@@ -4,19 +4,35 @@ import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/Footer";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Services from "./components/pages/Services";
+import { useState } from "react";
 
 const App = () => {
   // jsx fragmentation
 
+
+  const [username  , setUsername] = useState("tehleem")
+  const [darkMode, setDarkMode] = useState(false);
+
+
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+       {/* // passed username as a prop */}
+        <Navbar username = {username} darkMode = {darkMode} setDarkMode = {setDarkMode}/>   
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+
+        <div className={darkMode ? "bg-neutral-900 h-150 text-white" : "bg-neutral-400 h-150"}>
+
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+        </div>
+
+
 
         <Footer />
       </BrowserRouter>
--- P8_React.js/src/components/shared/Navbar.jsx
+++ P8_React.js/src/components/shared/Navbar.jsx
@@ -4,18 +4,25 @@ import { GiNightSleep } from "react-icons/gi";
 import { RiSunLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar(props) {
+
+
+
   const [isOpen, setIsOpen] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+
+
 
   return (
     <nav
       className={
-        darkMode
+        props.darkMode
           ? `bg-indigo-950 text-white px-6 py-4 flex items-center justify-between`
           : `bg-indigo-100 text-black px-6 py-4 flex items-center justify-between`
       }
     >
+
+
+
       <div className="text-2xl font-bold">MyBrand</div>
 
       <div>
@@ -23,20 +30,31 @@ export default function Navbar() {
           <li> <Link to="/">Home </Link> </li>
           <li>  <Link to="/about">  About </Link>  </li>
           <li> Contact</li>
-          <li> Services</li>
+          <li> <Link to="/services">Services </Link> </li>
         </ul>
       </div>
 
+
+
+
+
       <div>
+
+
+        <p> Welcome {props.username}</p>
+
+
         <button
           className="mx-7 hidden sm:block "
           onClick={() => {
-            setDarkMode(!darkMode);
-            console.log("dark mode" + " : " + darkMode);
+            props.setDarkMode(!props.darkMode);
+            // console.log("dark mode" + " : " + darkMode);
           }}
         >
-           <span className="text-xl">{darkMode ? <RiSunLine/> : <GiNightSleep/>}  </span>  
+           <span className="text-xl">{props.darkMode ? <RiSunLine/> : <GiNightSleep/>}  </span>  
         </button>
+
+
 
         <button
           className="mx-7 sm:hidden focus:outline-none"
@@ -47,7 +65,13 @@ export default function Navbar() {
         >
           <GiHamburgerMenu style={{ fontSize: 22 }} />
         </button>
+
+
       </div>
+
+
+
+
     </nav>
   );
 }
